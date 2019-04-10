b5dbdd14f96e25c6ee0bfda640252cb98d8ac4a7
Mateusz <mateuszdyrdol@gmail.com>
2019-04-10 22:43:31 +0200

    main updated

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-""" #to show the number
-image_index = 7772# You may select anything up to 60,000
-print(y_train[image_index]) # The label is 8
-plt.imshow(x_train[image_index], cmap='Greys')
-plt.show()
-"""
+
+
+# Function to display MNIST bitmap
+def display(i):
+    img = x_test[i]
+    plt.title('Example %d. Label: %d' % (i, y_test[i]))
+    plt.imshow(img.reshape((28,28)), cmap=plt.cm.gray_r)
+
 
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
+
 # Making sure that the values are float so that we can get decimal points after division
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+
 # Normalizing the RGB codes by dividing it to the max RGB value.
 x_train /= 255
 x_test /= 255
