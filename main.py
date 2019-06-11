d2d99c43126f89ca6a1c5071b8a14851512269c6
Mateusz <mateuszdyrdol@gmail.com>
2019-06-11 20:19:08 +0200

    final version

--- main.py
+++ main.py
@@ -1,146 +1,129 @@
-from __future__ import division, print_function, absolute_import
+from __future__ import absolute_import, division, print_function
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from matplotlib import pyplot as plt
 import numpy as np
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.keras import Model, layers
+from tensorflow.keras.datasets import mnist
 
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = np.array(x_train, np.float32), np.array(x_test, np.float32)
+x_train, x_test = x_train / 255., x_test / 255.
 
-# training parameters
+# training parameters.
+num_classes = 10
 learning_rate = 0.001
-num_steps = 300
+training_steps = 200
 batch_size = 128
 display_step = 10
 
-# network parameters
-num_input = 784  # MNIST data input (img shape: 28*28)
-num_classes = 10  # 0-9 digits
-dropout = 0.75  # dropout, probability to keep units
+# slice data
+train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
 
-# placeholders
-X = tf.placeholder(tf.float32, [None, num_input])
-Y = tf.placeholder(tf.float32, [None, num_classes])
-keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
-
-
-# function wrappers
-def conv2d(x, W, b, strides=1):
-    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
-    x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x)
+# network parameters.
+conv1_filters = 32 # number of filters for 1st conv layer.
+conv2_filters = 64 # number of filters for 2nd conv layer.
+fc1_units = 1024 # number of neurons for 1st fully-connected layer.
 
 
-def maxpool2d(x, k=2):
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
-                          padding='SAME')
+class ConvNet(Model):
+    # Set layers.
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        # Convolution Layer with 32 filters and a kernel size of 5.
+        self.conv1 = layers.Conv2D(32, kernel_size=5, activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with kernel size of 2 and strides of 2.
+        self.maxpool1 = layers.MaxPool2D(2, strides=2)
 
+        # Convolution Layer with 64 filters and a kernel size of 3.
+        self.conv2 = layers.Conv2D(64, kernel_size=3, activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with kernel size of 2 and strides of 2.
+        self.maxpool2 = layers.MaxPool2D(2, strides=2)
 
-def gen_image(arr):
-    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
-    plt.imshow(two_d, interpolation='nearest')
-    return plt
+        # Flatten the data to a 1-D vector for the fully connected layer.
+        self.flatten = layers.Flatten()
 
+        # Fully connected layer.
+        self.fc1 = layers.Dense(1024)
+        # Apply Dropout (if is_training is False, dropout is not applied).
+        self.dropout = layers.Dropout(rate=0.5)
 
-# create model
-def conv_net(x, weights, biases, dropout):
-    # MNIST data input is a 1-D vector of 784 features (28*28 pixels)
-    # Reshape to match picture format [Height x Width x Channel]
-    # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
-    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+        # Output layer, class prediction.
+        self.out = layers.Dense(num_classes)
 
-    # Convolution Layer
-    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
-    # Max Pooling (down-sampling)
-    conv1 = maxpool2d(conv1, k=2)
+    # Set forward pass.
+    def call(self, x, is_training=False):
+        x = tf.reshape(x, [-1, 28, 28, 1])
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.dropout(x, training=is_training)
+        x = self.out(x)
+        if not is_training:
+            # tf cross entropy expect logits without softmax, so only
+            # apply softmax when not training.
+            x = tf.nn.softmax(x)
+        return x
 
-    # Convolution Layer
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
-    # Max Pooling (down-sampling)
-    conv2 = maxpool2d(conv2, k=2)
+# network model
+conv_net = ConvNet()
 
-    # Fully connected layer
-    # Reshape conv2 output to fit fully connected layer input
-    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
-    fc1 = tf.nn.relu(fc1)
-    # dropout
-    fc1 = tf.nn.dropout(fc1, dropout)
+def cross_entropy_loss(x, y):
+    # Convert labels to int 64 for tf cross-entropy function.
+    y = tf.cast(y, tf.int64)
+    # Apply softmax to logits and compute cross-entropy.
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=x)
+    # Average loss across the batch.
+    return tf.reduce_mean(loss)
 
-    # output
-    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-    return out
+# Accuracy metric.
+def accuracy(y_pred, y_true):
+    # Predicted class is the index of highest score in prediction vector (i.e. argmax).
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
+    return tf.reduce_mean(tf.cast(correct_prediction, tf.float32), axis=-1)
 
+# Stochastic gradient descent optimizer.
+optimizer = tf.optimizers.Adam(learning_rate)
 
-# weight & bias
-weights = {
-    # 5x5 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-    # 5x5 conv, 32 inputs, 64 outputs
-    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-    # fully connected, 7*7*64 inputs, 1024 outputs
-    'wd1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
-    # 1024 inputs, 10 outputs (class prediction)
-    'out': tf.Variable(tf.random_normal([1024, num_classes]))
-}
+def run_optimization(x, y):
+    # Wrap computation inside a GradientTape for automatic differentiation.
+    with tf.GradientTape() as g:
+        # Forward pass.
+        pred = conv_net(x, is_training=True)
+        # Compute loss.
+        loss = cross_entropy_loss(pred, y)
 
-biases = {
-    'bc1': tf.Variable(tf.random_normal([32])),
-    'bc2': tf.Variable(tf.random_normal([64])),
-    'bd1': tf.Variable(tf.random_normal([1024])),
-    'out': tf.Variable(tf.random_normal([num_classes]))
-}
+    # Variables to update, i.e. trainable variables.
+    trainable_variables = conv_net.trainable_variables
 
-# model
-logits = conv_net(X, weights, biases, keep_prob)
-prediction = tf.nn.softmax(logits)
+    # Compute gradients.
+    gradients = g.gradient(loss, trainable_variables)
 
-# loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    logits=logits, labels=Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-train_op = optimizer.minimize(loss_op)
+    # Update W and b following gradients.
+    optimizer.apply_gradients(zip(gradients, trainable_variables))
 
-# model outputs
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+# Run training for the given number of steps.
+for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
+     # Run the optimization to update W and b values.
+    run_optimization(batch_x, batch_y)
+    if step % display_step == 0:
+        pred = conv_net(batch_x)
+        loss = cross_entropy_loss(pred, batch_y)
+        acc = accuracy(pred, batch_y)
+        print("step: %i, loss: %f, accuracy: %f" % (step, loss, acc))
 
-init = tf.global_variables_initializer()
+pred = conv_net(x_test)
+print("Test Accuracy: %f" % accuracy(pred, y_test))
 
-# training
-with tf.Session() as sess:
-    sess.run(init)
-    for step in range(1, num_steps + 1):
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # run
-        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y, keep_prob: 0.8})
-        if step % display_step == 0 or step == 1:
-            # calculate batch loss and accuracy
-            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                                 Y: batch_y,
-                                                                 keep_prob: 1.0})
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.3f}".format(acc))
+n_images = 20
+test_images = x_test[5:n_images+5]
+predictions = conv_net(test_images)
 
-    print("Optimization Finished!")
-
-    # test
-    print("Testing Accuracy:", \
-          sess.run(accuracy, feed_dict={X: mnist.test.images[:1000],
-                                        Y: mnist.test.labels[:1000],
-                                        keep_prob: 1.0}))
-
-    # batch_xs, batch_ys = mnist.test.next_batch(2)
-    # gen_image(batch_xs[0]).show()
-    # gen_image(batch_xs[1]).show()
-
-    n_images = 5
-    test_images = mnist.test.images[:n_images]
-    predictions = conv_net(test_images, weights, biases, keep_prob)
-
-    # Display image and model prediction.
-    for i in range(n_images):
-        plt.imshow(np.reshape(test_images[i], [28, 28]), cmap='gray')
-        plt.show()
-        print("Model prediction: %s" % str(tf.argmax(predictions, 1)))
+# Display image and model prediction.
+for i in range(n_images):
+    plt.imshow(np.reshape(test_images[i], [28, 28]), cmap='gray')
+    plt.show()
+    print("Model prediction: %i" % np.argmax(predictions.numpy()[i]))
